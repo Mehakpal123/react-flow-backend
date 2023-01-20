490c907f163a98c584afa7d7bb97fcfd6bd9a0a2
Farzpal <113582889+Mehakpal123@users.noreply.github.com>
2023-01-20 10:22:04 +0530

    Added "get-flow-data" route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,4 +3,5 @@ const UserController = require('../controllers/UserController');
 module.exports = function (app) {
 	app.post('/signup', UserController.signup);
 	app.post('/login', UserController.login);
+	app.get('/get-flow-data', UserController.login);
 }
